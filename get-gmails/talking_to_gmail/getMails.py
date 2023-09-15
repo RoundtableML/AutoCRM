c8dc40281439241cc39488717bb695c9c53ecb02
Caden Midkiff <cadenmidkiff32@gmail.com>
2023-09-14 18:48:26 -0700

    Implemented mail dict. Improved email parsing.

--- get-gmails/talking_to_gmail/getMails.py
+++ get-gmails/talking_to_gmail/getMails.py
@@ -1,27 +1,45 @@
 import imapclient
 import pyzmail
 
+# Initially followed this https://replit.com/talk/learn/How-to-Make-a-Python-Email-Bot/8194
 
-# I followed this https://replit.com/talk/learn/How-to-Make-a-Python-Email-Bot/8194
-
-def getMails(username, password):
-    i = imapclient.IMAPClient('imap.gmail.com')
+def getMails(username, password, folderName):
+    server = imapclient.IMAPClient('imap.gmail.com')
 
     # Username is email address
     # Password is an 'application specific' password (https://support.google.com/mail/answer/185833?hl=en)
+    server.login(username, password)
 
-    i.login(username, password)
-
-    # Note: When this reads your mails, it will mark them as read. Unless you want to mark all your emails as read,
-    # you should pick a folder besides "INBOX".  I created a folder (label) in gmail named 'Jobs'.
-    i.select_folder('Jobs')
-    uids = i.search()
-    rawmsgs = i.fetch(uids, ['BODY[]'])
-
+    server.select_folder('Jobs')
+    mail_ids = server.search() # Will mark emails as read.
+    rawmsgs = server.fetch(mail_ids, ['BODY[]'])
     mails = []
-    for uid in uids:
-        msg = pyzmail.PyzMessage.factory(rawmsgs[uid][b"BODY[]"])
+    for id in mail_ids:
+        msg = pyzmail.PyzMessage.factory(rawmsgs[id][b"BODY[]"])
         if msg.text_part:
-            mail = msg.text_part.get_payload().decode(msg.text_part.charset)
+            subject = msg.get_subject()
+            sender = list(msg.get_address('from'))
+            recipient = list(msg.get_address('to'))
+            body = msg.text_part.get_payload().decode(msg.text_part.charset)
+            body = " ".join(body.split()).replace(">", "").replace("&#39;", "'").replace("&rsquo;", "'")
+
+            mail = {
+                "subject": subject,
+                "sender": {
+                    "name": sender[0],
+                    "address": sender[1]
+                },
+                "recipient": {
+                    "name": recipient[0],
+                    "address": recipient[1]
+                },
+                "body": body,
+            }
+
             mails.append(mail)
+            print("$$$$$$$$$$$$$$$$$$$$$$$$$")
+            print("Subject: ", subject, "From: ", sender[1], "To: ", recipient[1])
+            print("id", id)
+            print(body)
+            print("$$$$$$$$$$$$$$$$$$$$$$$$$")
     return mails
--- get-gmails/talking_to_gmail/main.py
+++ get-gmails/talking_to_gmail/main.py
@@ -1,11 +1,11 @@
-import imapclient # Fetches email
-import pyzmail # Parses the email into readable text
-
 import secrets
 import llmJSONs
-import fun_with_sheets # Decided to use gspread instead of sheets api
+
 import getMails
-import fun_with_gspread # Decided to use gspread instead of sheets api
+import fun_with_gspread
+
+# import fun_with_sheets # Decided to use gspread instead of sheets api
+
 
 
 
@@ -14,28 +14,30 @@ import fun_with_gspread # Decided to use gspread instead of sheets api
 
 # Main has 3 steps
 def main():
-    # Step 1 - Get mails from gmail
-    mails = getMails.getMails(secrets.username, secrets.password)
+    #Step 1 - Get mails from gmail
+    mails = getMails.getMails(secrets.username, secrets.password, "Jobs")
     for mail in mails:
-        print(mail)
+        print("got mail!", (mail["body"][:10]))
 
 
 
-    # Step 2 - TODO - Programmatically call AI to analyze mails and return JSON
 
 
-    # Step 3 - Take the AI-generated JSON, add it to google sheets using gspread
-    next_open_row = 0
-
-    rows = []
-    for res in llmJSONs.responses:
-        if res['isRelatedToJob']:
-            row = []
-            row.append(res['companyName'])
-            row.append(res['applicationStatus'])
-            row.append(res['roleName'])
-            rows.append(row)
-    fun_with_gspread.add_rows('test sheet 2', rows)
+    # # Step 2 - TODO - Programmatically call AI to analyze mails and return JSON
+    #
+    #
+    # # Step 3 - Take the AI-generated JSON, add it to google sheets using gspread
+    # next_open_row = 0
+    #
+    # rows = []
+    # for res in llmJSONs.responses:
+    #     if res['isRelatedToJob']:
+    #         row = []
+    #         row.append(res['companyName'])
+    #         row.append(res['applicationStatus'])
+    #         row.append(res['roleName'])
+    #         rows.append(row)
+    # fun_with_gspread.add_rows('test sheet 2', rows)
 
 
 
